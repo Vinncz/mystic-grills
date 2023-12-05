dd8dc1c053629e4b10d27d65d64774c6f4c0ec5c
Vin <kevgun03@gmail.com>
2023-12-05 22:00:04 +0700

    Refactored ValidationMessages into ValidationState

---
+++ src/values/strings/ValidationState.java
@@ -0,0 +1,52 @@
+package values.strings;
+
+public enum ValidationState {
+    EMPTY_USERNAME
+        ("Username must be filled"),
+
+    EMPTY_EMAIL
+        ("Email must be filled"),
+
+    DUPLICATE_EMAIL
+        ("Said email has an account associated with it"),
+
+    EMPTY_PASSWORD
+        ("Password must be filled"),
+
+    INVALID_PASSWORD_LENGTH
+        ("Password must at least be longer than 6 characters"),
+
+    EMPTY_CONFIRMATION_PASSWORD
+        ("Confirmation password must be filled"),
+
+    INCORRECT_CONFIRMATION_PASSWORD
+        ("Confirmation password must be the same as your password"),
+
+    UNREGISTERED_EMAIL
+        ("Said email has no account associated with it. Try registering first"),
+
+    INCORRECT_PASSWORD
+        ("Incorrect password entered"),
+
+    GENERIC_INVALID_ARGUMENT
+        ("Invalid input!");
+
+    public String value;
+    private ValidationState (String _value) {
+        this.value = _value;
+    }
+
+    public static ValidationState find (String _value) {
+        for (ValidationState state : ValidationState.values()) {
+            if (state.value.equals(_value)) {
+                return state;
+            }
+        }
+        return null;
+    }
+
+    public String value () {
+        return this.value;
+    }
+
+}
