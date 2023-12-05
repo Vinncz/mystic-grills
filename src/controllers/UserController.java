538f3eebe4432189e84759c3244a8c25a002c7cd
Vin <kevgun03@gmail.com>
2023-12-05 22:02:20 +0700

    Added Javadoc for UserController, implemented ValidationController

--- src/controllers/UserController.java
+++ src/controllers/UserController.java
@@ -5,29 +5,64 @@ import java.util.Optional;
 
 import models.User;
 import repositories.UserRepository;
+import values.strings.ValidationState;
 
 public class UserController {
 
     private UserRepository userRepo;
+    private ValidationController validator;
 
     public UserController () {
         this.userRepo = new UserRepository();
+        this.validator = new ValidationController();
 
     }
 
-    public User authenticateUser (String _userEmail, String _userPassword) {
+    /**
+     * Authenticates a user by validating the provided email and password.
+     *
+     * @param _userEmail • The email of the user to authenticate.
+     * @param _userPassword • The password associated with the user's account.
+     * @return An instance of {@code AuthenticationReturnDatatype}, which indicates the whether the validation is successfully done or not through its state; and whether there are any associated account if the state is null.
+     */
+    public UserRepository.AuthenticationReturnDatatype authenticateUser (String _userEmail, String _userPassword) {
+        UserRepository.AuthenticationReturnDatatype returnObject = new UserRepository.AuthenticationReturnDatatype();
+
+        if ( validator.isBlank(_userEmail) || validator.isBlank(_userPassword) ) {
+            if ( validator.isBlank(_userEmail) )
+                returnObject.setState(ValidationState.EMPTY_EMAIL);
+            else if ( validator.isBlank(_userPassword) )
+                returnObject.setState(ValidationState.EMPTY_PASSWORD);
+
+            return returnObject;
+        }
+
+        if ( validator.lessThanNCharacters(_userPassword, 6) ) {
+            returnObject.setState(ValidationState.INVALID_PASSWORD_LENGTH);
+
+            return returnObject;
+        }
+
         return userRepo.authenticateUser(_userEmail, _userPassword);
 
     }
 
+    /**
+     * Communicates with the {@code UserRepository} to bring you the associated account for the given email (if any)
+     * @param _userEmail
+     */
     public Optional<User> getUserByEmail(String _userEmail) {
         return userRepo.getUserByEmail(_userEmail);
 
     }
 
+    /**
+     * Communicates with the {@code UserRepository} to bring you the associated account for the given email (if any)
+     * @param _userEmail
+     */
     public Optional<User> getById (Integer _id) {
         return userRepo.getById(_id);
-        
+
     }
 
     public ArrayList<User> getAll () {
---
+++ src/controllers/ValidationController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+public class ValidationController {
+
+    public Boolean lessThanNCharacters (String _s, Integer _n) {
+        return _s.length() < _n ? true : false;
+    }
+
+    public Boolean lessThanOrEqualsToNCharacters (String _s, Integer _n) {
+        return _s.length() <= _n ? true : false;
+    }
+
+    public Boolean longerThanNCharacters (String _s, Integer _n) {
+        return _s.length() > _n ? true : false;
+    }
+
+    public Boolean longerThanOrEqualsToNCharacters (String _s, Integer _n) {
+        return _s.length() >= _n ? true : false;
+    }
+
+    public Boolean isBlank (String _s) {
+        return _s.isBlank();
+    }
+
+}
